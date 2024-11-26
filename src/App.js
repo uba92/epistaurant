08b94de3269843de88fcd18d9e55627648ae4154
Umberto Tramontano <u.tramontano92@libero.it>
2024-11-26 12:16:12 +0100

    adds carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg'
 import './App.css'
 //così allego il file css di bootstrap a tutti i file
 import 'bootstrap/dist/css/bootstrap.min.css'
+import MainContent from './components/MainContent'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='App-header'></header>
+      <main className='bg-dark'>
+        <Container>
+          <Row className='justify-content-center'>
+            <Col xs={12} md={8} lg={6}>
+              <MainContent />
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </div>
   )
 }
